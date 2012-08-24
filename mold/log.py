3b6b7b1cdcf0c06765f1b7f02378717b14cc5a82
Matt Haggard <haggardii@gmail.com>
2012-08-24 11:55:15 -0600

    Remove sending function from Logger
    Now it's just a message maker

--- mold/log.py
+++ mold/log.py
@@ -1,34 +1,25 @@
 
 
 
-class Logger(object):
+class MessageFactory(object):
     """
-    I provide a common set of messages to send to a Historian.
+    I produce a common set of messages to send to a Historian.
     """
-    
-    def sendMessage(self, message):
-        """
-        Send a message along... whatever that means for you.
-        
-        @type message: dict
-        @param message: Dictionary of data to send along.
-        """
-        raise NotImplementedError()
-    
+
     
     def fd(self, label, fd, data):
-        self.sendMessage({
+        return {
             'lab': label,
             'ev': 'fd',
             'fd': fd,
             'm': data,
-        })
+        }
 
 
     def processEnded(self, label, exitCode, signal):
-        self.sendMessage({
+        return {
             'lab': label,
             'ev': 'pexit',
             'exitCode': exitCode,
             'signal': signal,
-        })
+        }
--- mold/test/test_log.py
+++ mold/test/test_log.py
@@ -1,39 +1,19 @@
 from twisted.trial.unittest import TestCase
 
 
-from mold.log import Logger
+from mold.log import MessageFactory
 
 
-class MemoryLogger(Logger):
-
-    def __init__(self):
-        self.messages = []
-
-
-    def sendMessage(self, message):
-        self.messages.append(message)
-
-
-
-class LoggerTest(TestCase):
-
-
-    def test_sendMessage(self):
-        """
-        You have to subclass and implement the message sending.
-        """
-        l = Logger()
-        self.assertRaises(NotImplementedError, l.sendMessage, 'foo')
+class MessageFactoryTest(TestCase):
 
 
     def test_fd(self):
         """
         You can signal bytes moving over a file descriptor
         """
-        l = MemoryLogger()
-        l.fd('label', 0, 'hello')
+        f = MessageFactory()
+        m = f.fd('label', 0, 'hello')
         
-        m = l.messages[-1]
         self.assertEqual(m['ev'], 'fd')
         self.assertEqual(m['fd'], 0)
         self.assertEqual(m['lab'], 'label')
@@ -44,10 +24,9 @@ class LoggerTest(TestCase):
         """
         You can signal that a process ended
         """
-        l = MemoryLogger()
-        l.processEnded('label', 1, 2)
-        
-        m = l.messages[-1]
+        f = MessageFactory()
+        m = f.processEnded('label', 1, 2)
+
         self.assertEqual(m['ev'], 'pexit')
         self.assertEqual(m['lab'], 'label')
         self.assertEqual(m['exitCode'], 1)
